d6e922d76ce007fea6766d1660eb3921d70ba5ba
Maximiliano <maxponmar.marquez@gmail.com>
2020-06-05 01:04:48 -0700

    Finished

--- PID-Test/PID-Test.ino
+++ PID-Test/PID-Test.ino
@@ -1,56 +1,83 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <PID_v1.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2);  
 // Variables
-int sensorInput;                    
-float temp;                         
+int sensorInput;                                            
 int potInput;                       
-float Vout= 0;                      
-float R1= 10000;                    
-float R2= 0;                        
-float buffer= 0;  
+double Vout= 0;                      
+double R1= 10000;                    
+double R2= 0;                        
+double buffer= 0;  
 
-//int temporal=0;  
+// INPUT
+double tempInput; 
+// Output
+double tempOutput;
+double pidOutput;
+
+int salidaFoco;
+int salidaAbanico;
+
+// PID
+double Kp=54, Ki=13, Kd=5; 
+PID myPID(&tempOutput, &pidOutput, &tempInput, Kp, Ki, Kd, DIRECT);
+ 
 void setup()
 {
-  //temporal=0;
+  Serial.begin(9600);
+  
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("  Temperatura  ");
+
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetTunings(Kp, Ki, Kd);
 }
 
 
 void loop()
 {
-  /*if (temporal==255)
-    temporal-=10;
-  else
-    temporal+=10;
-  analogWrite(2, temporal);
-  delay(500);*/
+  //analogWrite(2, 255);
   sensorInput = analogRead(0);
-  temp = (float)sensorInput / 1024;
-  temp *= 4.8;
-  temp -= 0.5;
-  temp *= 100;
+  tempOutput = (float)sensorInput / 1024;
+  //temp = ((temp*4.7)-0.5)*100;
+  tempOutput *= 4.7;
+  tempOutput -= 0.5;
+  tempOutput *= 100;
   lcd.setCursor(0,1);
-  lcd.print(temp);
-
+  lcd.print(tempOutput);
+  
   potInput= analogRead(1);
   if(potInput) {
   buffer= potInput * 5;
   Vout= (buffer)/1024.0;
   buffer= (5/Vout) -1;
   R2= R1 * buffer; }
-  R2=0.0014*R2+22.95;
-
+  tempInput=0.0028*R2+31.86;
   lcd.setCursor(7, 1);
   lcd.print("I:");
-  lcd.print(R2);
+  lcd.print(tempInput);
 
-  // PID HERE!!!
+  myPID.Compute();
+
+  salidaFoco = (int)pidOutput;
+  salidaAbanico = 255-salidaFoco;
+
+  analogWrite(2, salidaFoco);
+  analogWrite(3, salidaAbanico);
+  
+  String msg1 = "Temp deseada: ";
+  String msg2 = "Temp actual: ";
+  String msg3 = "Salida Foco: ";
+  String msg4 = "Salida Abanico: ";
+  Serial.println(msg1 + tempInput);
+  Serial.println(msg2 + tempOutput);
+  Serial.println(msg3 + salidaFoco);
+  Serial.println(msg4 + salidaAbanico);
+  Serial.println("");
   
   delay(1000);
 }
